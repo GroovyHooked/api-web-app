0549fcbc1e4730c5237ccc0fb5176684ac706950
GroovyHooked <dafrenchie2002@yahoo.fr>
2022-03-09 23:48:58 +0100

    Linking planets to planet residents

--- src/components/planets/Planets.js
+++ src/components/planets/Planets.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { PlanetsResident } from "./PlanetsResident";
 import axios from "axios";
 
 export const Planets = () => {
   const [fetchData, setFetchData] = useState({});
   const [fetchData1, setFetchData1] = useState({});
   const [fetchData2, setFetchData2] = useState({});
-  const [urlRecover, setUrlRecover] = useState({});
+  const [urlRecover, setUrlRecover] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [isLoaded1, setIsLoaded1] = useState(false);
+
 
   const url = "https://swapi.dev/api/planets/";
   const peopleUrl = "https://swapi.dev/api/people/";
@@ -19,63 +18,46 @@ export const Planets = () => {
       .get(url)
       .then((resp) => {
         setFetchData(resp.data);
-        console.log("fetchData1 => ", fetchData1);
+        console.log("fetchData1 => ", resp.data);
         setIsLoaded(true);
       })
       .catch(function (error) {
         console.log(error);
       });
 
-    axios
-      .get(url)
-      .then((resp) => {
-        setFetchData1(resp.data);
-        console.log("fetchData1 => ", fetchData1);
-        resp.data.results.forEach(
-          (e) =>
-            setUrlRecover(urlRecover.push(e.residents)) &&
-            console.log(e.residents)
-        );
-
-        console.log("urlRecover => ", urlRecover);
-        setIsLoaded1(true);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
-    axios
-      .get(peopleUrl)
-      .then((resp) => {
-        setFetchData2(resp.data);
-        setIsLoaded1(true);
-        console.log("resp.data", resp.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
   }, []);
 
-  if (isLoaded && isLoaded1) {
-    return fetchData.results.map((res, i) => {
+   const getName = (url, id) => {
+    axios.get(url + id + '/')
+          .then((res) => {
+            console.log("res => "+res.data.name)
+            return res.data.name
+          })
+  }
+
+
+  if (isLoaded ) {
+    return fetchData.results.map((planet, i) => {
       return (
         <div key={i++}>
-          <h4>{res.name}</h4>
-          <p>Rotation period: {res.rotation_period}</p>
-          <p>Climate: {res.climate}</p>
-          <p>Terrain: {res.terrain}</p>
-          <p>Population: {res.population}</p>
+          <h4>{planet.name}</h4>
+          <p>Rotation period: {planet.rotation_period}</p>
+          <p>Climate: {planet.climate}</p>
+          <p>Terrain: {planet.terrain}</p>
+          <p>Population: {planet.population}</p>
           <div>
             <h5>Résidents:</h5>
-            <ul>
-              {fetchData2.results.map((res, i) => {
+            {<ul>
+              {planet.residents.map((res, i) => {
+                let splitUrl = res.split("/");
+                let id = splitUrl[splitUrl.length - 2];
                 return (
                   <li key={i}>
-                    <a href={"/resident/" + (i + 1)}>{res.name}</a>
+                    <a href={"/resident/" + (id)}>{res}</a>
                   </li>
                 );
               })}
-            </ul>
+            </ul>}
           </div>
         </div>
       );
